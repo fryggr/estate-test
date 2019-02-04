11e5d992abe79378a3ed65ff899222f739d75c61
Daria Sminova <daria@realrussialimited.local>
2019-02-04 15:54:15 +0400

    fix

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react'
 import { Col } from 'react-materialize'
+import './index.css'
 
 class Card extends Component {
     render() {
-        const card = this.props.card
+        const card = this.props.card;
+        console.log(card.specification.area);
         return (
-            <Col s={4}>
+            <Col s={3}>
                 <div className="cards__item">
-                    <img src="" alt="estate" className="cards__image"/>
-                    <div className="cards__title">{card.location.settlementName}</div>
-                    <div className="cards__price">{card.saleOffer === null ? card.rentOffer === null ? null : card.rentOffer.price : card.saleOffer.price}</div>
-                    <div className="cards__area">{card.landDetails.area}</div>
+                    <div className="cards__image"></div>
+                    <div className="cards__title grey-text text-darken-2">{card.location.settlementName}</div>
+                    <h5 className="cards__price">{
+                        card.saleOffer === null ?
+                        card.rentOffer === null ?
+                        null :
+                        `$${card.rentOffer.multiCurrencyPrice.usd.toLocaleString('ru')}` :
+                        `$${card.saleOffer.multiCurrencyPrice.usd.toLocaleString('ru')}`
+                    }
+                    </h5>
+                    <div className="cards__area">
+                        <div style={{'marginRight': '30px'}} className="valign-wrapper">
+                            <i class="tiny material-icons">map</i>
+                            <span>{`${parseInt(card.landDetails.area)} сот`}</span>
+                        </div>
+                        {card.specification.area !== null ?
+                            (
+                                <div className="valign-wrapper">
+                                    <i class="tiny material-icons">home</i>
+                                    <span>{`${parseInt(card.specification.area)} м`}<sup>2</sup></span>
+                                </div>
+                            )
+                            : ''
+                        }
+
+                    </div>
                 </div>
             </Col>
         )
