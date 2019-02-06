443801dde674acbb795cfcc9a394ae3f400dc3e6
Daria Sminova <daria@realrussialimited.local>
2019-02-06 17:31:05 +0400

    add redux dev tools

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,13 @@ import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { createLogger } from 'redux-logger'
 import reducer from '../reducers'
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const logger = createLogger()
 
 const store = createStore(
-    reducer,
-    applyMiddleware(thunk, logger)
+    reducer, composeWithDevTools(
+    applyMiddleware(thunk, logger))
 )
 
 window.store = store
